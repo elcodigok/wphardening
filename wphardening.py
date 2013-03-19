e934fbe8dd2b44fac0cee29373d014caf2596f42
Daniel M. Maldonado <danyx@elcodigok.com.ar>
2013-03-19 13:55:01 -0300

    Nuevas agrupaciones en el menu.

--- wphardening.py
+++ wphardening.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from optparse import OptionParser
+from optparse import OptionParser, OptionGroup
 from lib.checkWordpress import checkWordpress
 from lib.chmodWordPress import chmodWordPress
 from lib.removeWordPress import removeWordPress
@@ -12,9 +12,19 @@ def main():
 	version = '\nWP Hardening v0.1 (beta)\n'
 	parser = OptionParser(usage, version=version)
 	parser.add_option("-v", "--verbose", action="store_true", dest="verbose", help="active verbose mode output results")
-	parser.add_option("-d", "--dir", dest="path", help="**REQUIRED** - Working Directory.", metavar="DIRECTORY")
-	parser.add_option("-c", "--chmod-directory", action="store_true", dest="chmod", help="Chmod 755 in directory and 644 in files.")
-	parser.add_option("-r", "--remove", action="store_true", dest="remove", help="Remove files and directory.")
+	
+	group1 = OptionGroup(parser, "Target", "This option must be specified to modify the package WordPress.")
+	group1.add_option("-d", "--dir", dest="path", help="**REQUIRED** - Working Directory.", metavar="DIRECTORY")
+	parser.add_option_group(group1)
+	
+	group2 = OptionGroup(parser, "Hardening", "Different tools to hardening WordPress.")
+	group2.add_option("-c", "--chmod", action="store_true", dest="chmod", help="Chmod 755 in directory and 644 in files.")
+	group2.add_option("-r", "--remove", action="store_true", dest="remove", help="Remove files and directory.")
+	parser.add_option_group(group2)
+	
+	group3 = OptionGroup(parser, "Miscellaneous")
+	group3.add_option("--wizard", action="store_true", dest="wizard", help="Simple wizard interface for beginner users")
+	parser.add_option_group(group3)
 
 	(options, args) = parser.parse_args()
 
