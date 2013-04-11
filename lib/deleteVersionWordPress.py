a1f5cccc7aa5641bce79eed83776af5eb1a6c0cd
Daniel M. Maldonado <danyx@elcodigok.com.ar>
2013-04-11 09:36:33 -0300

    normas PEP8 a deleteVersionWordPress.py

--- lib/deleteVersionWordPress.py
+++ lib/deleteVersionWordPress.py
@@ -1,30 +1,39 @@
 import os
 from lib.termcolor import colored, cprint
 
+
 class deleteVersionWordPress():
-	def __init__(self, directory):
-		self.directory = os.path.abspath(directory)
-		self.filters = self.directory + "/wp-includes/default-filters.php"
-		self.setFunction()
-		self.setFilters()
-	
-	def setFilters(self):
-		self.f = open(self.filters, "r")
-		self.script = self.f.readlines()
-		self.f.close()
-	
-	def getFilters(self):
-		return self.script
-	
-	def setFunction(self):
-		self.function = ['\n', '// This is a function that removes versions of WordPress.\n', 'function delete_version_wp() {', '\n', '\treturn "";', '\n}', '\nadd_filter(\'the_generator\', \'delete_version_wp\');']
-	
-	def getFunction(self):
-		return self.function
-	
-	def delete(self):
-		f = open(self.filters, "w")
-		f.writelines(self.getFilters() + self.getFunction())
-		f.close()
-		print colored('\nDeleted WordPress versions', 'yellow')
-		print colored('\tmodified:\twp-includes/default-filters.php', 'red')
+    def __init__(self, directory):
+        self.directory = os.path.abspath(directory)
+        self.filters = self.directory + "/wp-includes/default-filters.php"
+        self.setFunction()
+        self.setFilters()
+
+    def setFilters(self):
+        self.f = open(self.filters, "r")
+        self.script = self.f.readlines()
+        self.f.close()
+
+    def getFilters(self):
+        return self.script
+
+    def setFunction(self):
+        self.function = [
+            '\n',
+            '// This is a function that removes versions of WordPress.\n',
+            'function delete_version_wp() {', '\n', '\treturn "";', '\n}',
+            '\nadd_filter(\'the_generator\', \'delete_version_wp\');',
+        ]
+
+    def getFunction(self):
+        return self.function
+
+    def delete(self):
+        f = open(self.filters, "w")
+        f.writelines(self.getFilters() + self.getFunction())
+        f.close()
+        print colored('\nDeleted WordPress versions', 'yellow')
+        print colored(
+            '\tmodified:\twp-includes/default-filters.php',
+            'red'
+        )
