de30aaf764523afa0983342850cafcc4fb32e668
Daniel M. Maldonado <danyx@elcodigok.com.ar>
2013-04-11 09:31:27 -0300

    normas PEP8 a chmodWordPress.py

--- lib/chmodWordPress.py
+++ lib/chmodWordPress.py
@@ -1,16 +1,17 @@
 import os
 from lib.termcolor import colored, cprint
 
+
 class chmodWordPress():
-	def __init__(self, directory):
-		self.directory = os.path.abspath(directory)
-	
-	def changePermisions(self):
-		for r, d, f in os.walk(self.directory):
-			os.chmod(r, 0755)
-			for wpfile in f:
-				os.chmod(os.path.join(r, wpfile), 0644)
-		print colored('\nchmod on Directories', 'yellow')
-		print colored('\tAll directories\t drwxr-xr-x (755)', 'green')
-		print colored('\nchmod on Files', 'yellow')
-		print colored('\tAll files\t -rw-r--r-- (644)', 'green')
+    def __init__(self, directory):
+        self.directory = os.path.abspath(directory)
+
+    def changePermisions(self):
+        for r, d, f in os.walk(self.directory):
+            os.chmod(r, 0755)
+            for wpfile in f:
+                os.chmod(os.path.join(r, wpfile), 0644)
+        print colored('\nchmod on Directories', 'yellow')
+        print colored('\tAll directories\t drwxr-xr-x (755)', 'green')
+        print colored('\nchmod on Files', 'yellow')
+        print colored('\tAll files\t -rw-r--r-- (644)', 'green')
