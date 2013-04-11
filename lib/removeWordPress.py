3351682b987f72a9a583262631a9670b233c7cae
Daniel M. Maldonado <danyx@elcodigok.com.ar>
2013-04-11 10:00:57 -0300

    normas PEP8 a removeWordPress.py

--- lib/removeWordPress.py
+++ lib/removeWordPress.py
@@ -1,24 +1,25 @@
 import os
 from lib.termcolor import colored, cprint
 
-class removeWordPress():
-	def __init__(self, directory):
-		self.directory = os.path.abspath(directory)
-		self.readme = "/readme.html"
-		self.license = ["/licencia.txt", "/license.txt"]
-	
-	def deleteReadme(self):
-		if os.path.exists(self.directory + self.readme):
-			os.remove(self.directory + self.readme)
-			print colored('\tdelete:\tfile readme.', 'red')
-	
-	def deleteLicense(self):
-		for pathLicese in self.license:
-			if os.path.exists(self.directory + pathLicese):
-				os.remove(self.directory + pathLicese)
-				print colored('\tdelete:\tfile ' + pathLicese, 'red')
 
-	def delete(self):
-		print colored('\nRemove files by defaults', 'yellow')
-		self.deleteReadme()
-		self.deleteLicense()
+class removeWordPress():
+    def __init__(self, directory):
+        self.directory = os.path.abspath(directory)
+        self.readme = "/readme.html"
+        self.license = ["/licencia.txt", "/license.txt"]
+
+    def deleteReadme(self):
+        if os.path.exists(self.directory + self.readme):
+            os.remove(self.directory + self.readme)
+            print colored('\tdelete:\tfile readme.', 'red')
+
+    def deleteLicense(self):
+        for pathLicese in self.license:
+            if os.path.exists(self.directory + pathLicese):
+                os.remove(self.directory + pathLicese)
+                print colored('\tdelete:\tfile ' + pathLicese, 'red')
+
+    def delete(self):
+        print colored('\nRemove files by defaults', 'yellow')
+        self.deleteReadme()
+        self.deleteLicense()
