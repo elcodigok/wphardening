3343288a213ebf26d97c2b232136499bf1fa545c
Daniel M. Maldonado <danyx@elcodigok.com.ar>
2013-04-11 09:39:55 -0300

    normas PEP8 a fingerprintingWordPress.py

--- lib/fingerprintingWordPress.py
+++ lib/fingerprintingWordPress.py
@@ -5,30 +5,36 @@ import re
 import datetime
 from lib.termcolor import colored, cprint
 
+
 class fingerprintingWordPress():
-	def __init__(self, directory):
-		self.directory = os.path.abspath(directory)
-		self.includes = ['*.js', '*.css'] # for files only
-		self.includes = r'|'.join([fnmatch.translate(x) for x in self.includes])
-	
-	def searchStaticFile(self):
-		for root, dirs, files in os.walk(self.directory):
-			dirs[:] = [os.path.join(root, d) for d in dirs]
+    def __init__(self, directory):
+        self.directory = os.path.abspath(directory)
+        # for files only
+        self.includes = ['*.js', '*.css']
+        self.includes = r'|'.join(
+            [fnmatch.translate(x) for x in self.includes]
+        )
 
-			# exclude/include files
-			files = [os.path.join(root, f) for f in files]
-			files = [f for f in files if re.match(self.includes, f)]
+    def searchStaticFile(self):
+        for root, dirs, files in os.walk(self.directory):
+            dirs[:] = [os.path.join(root, d) for d in dirs]
 
-			for fname in files:
-				f = open(fname, "r")
-				script = f.readlines()
-				f.close()
-				f = open(fname, "w")
-				f.writelines(self.getDateTime() + script)
-				f.close()
-		print colored('\nDeleted fingerprinting WordPress', 'yellow')
-		print colored('\tAll changes implemented.', 'green')
-	
-	def getDateTime(self):
-		self.now = ['/* WP Hardening - ', str(datetime.datetime.now()) + '*/\n']
-		return self.now
+            # exclude/include files
+            files = [os.path.join(root, f) for f in files]
+            files = [f for f in files if re.match(self.includes, f)]
+
+            for fname in files:
+                f = open(fname, "r")
+                script = f.readlines()
+                f.close()
+                f = open(fname, "w")
+                f.writelines(self.getDateTime() + script)
+                f.close()
+        print colored('\nDeleted fingerprinting WordPress', 'yellow')
+        print colored('\tAll changes implemented.', 'green')
+
+    def getDateTime(self):
+        self.now = [
+            '/* WP Hardening - ', str(datetime.datetime.now()) + '*/\n'
+        ]
+        return self.now
