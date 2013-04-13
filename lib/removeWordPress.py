3dce514d9a015e57dff561810f70cdf7d7ac01b7
Daniel Maldonado <daniel_5502@yahoo.com.ar>
2013-04-13 17:53:02 -0300

    Licencia GPLv3 a todos los archivos del codigo fuente.

--- lib/removeWordPress.py
+++ lib/removeWordPress.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python
+"""
+removeWordPress.py
+
+Copyright 2013 Daniel Maldonado
+
+This file is part of WPHardening project.
+
+WPHardening is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+WPHardening is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WPHardening.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 import os
 from lib.termcolor import colored, cprint
 
