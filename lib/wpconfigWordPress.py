b97c146bc2a6a09f882af2a02e2c7e114447f4b5
Daniel Maldonado <daniel_5502@yahoo.com.ar>
2013-04-25 07:02:57 -0300

    Nuevos metodos para la clase wpconfigWordPress().

--- lib/wpconfigWordPress.py
+++ lib/wpconfigWordPress.py
@@ -29,16 +29,38 @@ class wpconfigWordPress():
     def __init__(self, directory):
         self.directory = directory
         self.opener = urllib2.build_opener(urllib2.HTTPHandler)
+        self.db_host = 'localhost'
     
     def wizard(self):
-        self.db_name = raw_input('Name of the database > ')
-        self.db_user = raw_input('Name of the User > ')
-        self.db_password = raw_input('Password of the user > ')
+        self.setDbName()
+        self.setDbUser()
+        self.setDbPassword()
         self.db_host = raw_input('Host [localhost] > ')
         self.salt = self.getSalt()
-        self.table_prefix = raw_input('Table prefix [wp_] > ')
+        self.table_prefix = raw_input('Table prefix [wph_] > ')
         self.language = raw_input('Language [es_ES] > ')
         self.getCompletConfig()
+    
+    def setDbName(self):
+        value = raw_input('Name of the database > ')
+        if value == '':
+            self.setDbName()
+        else:
+            self.db_name = value
+
+    def setDbUser(self):
+        value = raw_input('Name of the User > ')
+        if value == '':
+            self.setDbUser()
+        else:
+            self.db_user = value
+
+    def setDbPassword(self):
+        value = raw_input('Password of the user > ')
+        if value == '':
+            self.setDbPassword()
+        else:
+            self.db_password = value
     
     def getCompletConfig(self):
         self.complet = """/** WordPress absolute path to the Wordpress directory. */
@@ -55,24 +77,69 @@ require_once(ABSPATH . 'wp-settings.php');
         html = resp.read()
         return html
 
+    def getComment(self, message):
+        return ('/**\n' + ' * %s.\n'+' */\n') % message
+
     def createConfig(self):
         f = open(self.directory + '/wp-config-wphardening.php', 'w')
         self.wizard()
         f.write('<?php \n\n')
-        f.write('define(\'DB_NAME\', \'' + self.db_name + '\');\n\n')
-        f.write('define(\'DB_USER\', \'' + self.db_user + '\');\n\n')
-        f.write('define(\'DB_PASSWORD\', \'' + self.db_password + '\');\n\n')
-        f.write('define(\'DB_HOST\', \'' + self.db_host + '\');\n\n')
-        f.write('define(\'DB_CHARSET\', \'utf8\');\n\n')
-        f.write('define(\'DB_COLLATE\', \'\');\n\n')
-        f.write(self.salt + '\n')
-        f.write('$table_prefix = \'' + self.table_prefix + '\';\n\n')
-        f.write('define(\'WPLANG\', \'' + self.language + '\');\n\n')
-        f.write('define(\'WP_DEBUG\', false);\n\n')
-        f.write('define(\'WP_POST_REVISIONS\', false);\n\n')
-        f.write('define(\'FS_METHOD\', \'direct\');\n\n')
-        f.write('define(\'AUTOSAVE_INTERNAL\', 240);\n\n')
-        f.write('define(\'DISALLOW_FILE_EDIT\', true);\n\n')
+        f.write(
+            self.getComment('The name of the database for WordPress') +
+            'define(\'DB_NAME\', \'' + self.db_name + '\');\n\n'
+        )
+        f.write(
+            self.getComment('MySQL database username') +
+            'define(\'DB_USER\', \'' + self.db_user + '\');\n\n'
+        )
+        f.write(
+            self.getComment('MySQL database password') +
+            'define(\'DB_PASSWORD\', \'' + self.db_password + '\');\n\n'
+        )
+        f.write(
+            self.getComment('MySQL hostname') +
+            'define(\'DB_HOST\', \'' + self.db_host + '\');\n\n'
+        )
+        f.write(
+            self.getComment('Database Charset to use in creating database tables.') +
+            'define(\'DB_CHARSET\', \'utf8\');\n\n'
+        )
+        f.write(
+            self.getComment('The Database Collate type. Don\'t change this if in doubt.') +
+            'define(\'DB_COLLATE\', \'\');\n\n'
+        )
+        f.write(
+            self.getComment('Authentication Unique Keys and Salts.') +
+            self.salt + '\n'
+        )
+        f.write(
+            self.getComment('WordPress Database Table prefix.') +
+            '$table_prefix = \'' + self.table_prefix + '\';\n\n'
+        )
+        f.write(
+            self.getComment('WordPress Localized Language, defaults to English.') +
+            'define(\'WPLANG\', \'' + self.language + '\');\n\n'
+        )
+        f.write(
+            self.getComment('For developers: WordPress debugging mode.') +
+            'define(\'WP_DEBUG\', false);\n\n'
+        )
+        f.write(
+            self.getComment('Disable the Revisions.') +
+            'define(\'WP_POST_REVISIONS\', false);\n\n'
+        )
+        f.write(
+            self.getComment('Change the Filesystem Method.') +
+            'define(\'FS_METHOD\', \'direct\');\n\n'
+        )
+        f.write(
+            self.getComment('Change the Autosave Interval.') +
+            'define(\'AUTOSAVE_INTERNAL\', 240);\n\n'
+        )
+        f.write(
+            self.getComment('Disable Editing of Plugin & Theme Files.') +
+            'define(\'DISALLOW_FILE_EDIT\', true);\n\n'
+        )
         f.write('define(\'DISALLOW_FILE_MODS\', true);\n\n')
         f.write(self.complet + '\n')
         f.close()
