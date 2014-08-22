9008ccb47dd6e9475e500987f76b6dd3cec553bd
Daniel Maldonado <dmaldonado@jujuy.gov.ar>
2014-08-22 12:40:40 -0300

    Add Docstring to lib/chmodWordPress.py

--- lib/chmodWordPress.py
+++ lib/chmodWordPress.py
@@ -27,11 +27,24 @@ from lib.termcolor import colored
 
 
 class chmodWordPress():
+    """
+    This class changes the permissions on files and directories
+    on a WordPress project.
+
+    :author: Daniel Maldonado (daniel_5502@yahoo.com.ar)
+    """
     def __init__(self, directory, verbose=False):
+        """
+        :param directory: Absolute path of the directory to check.
+        :param verbose: Mode verbose.
+        """
         self.directory = os.path.abspath(directory)
         self.mode_verbose = verbose
 
     def changePermisions(self):
+        """
+        :return: None
+        """
         for r, d, f in os.walk(self.directory):
             os.chmod(r, 0755)
             if self.mode_verbose:
