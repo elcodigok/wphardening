d78ea08eedced1a044f746bdfbf45b8f88dea676
Daniel Maldonado <daniel_5502@yahoo.com.ar>
2016-01-03 23:54:15 -0300

    The duplicate entry is removed.

--- lib/removeWordPress.py
+++ lib/removeWordPress.py
@@ -48,7 +48,6 @@ class removeWordPress():
         ]
         self.static_file = [
             "/wp-includes/images/crystal/license.txt",
-            "/wp-includes/images/crystal/license.txt",
             "/wp-includes/js/plupload/license.txt",
             "/wp-includes/js/plupload/changelog.txt",
             "/wp-includes/js/tinymce/license.txt",
