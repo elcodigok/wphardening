bb6e82dd1f43f49ad6add91c0234d1a9acb31d9d
Daniel Maldonado <daniel_5502@yahoo.com.ar>
2017-04-14 12:32:20 -0300

    Add WPS Hide Login plugins.

--- lib/pluginsWordPress.py
+++ lib/pluginsWordPress.py
@@ -98,6 +98,19 @@ class pluginsWordPress():
                 'https://wordpress.org/plugins/stream/'
             ],
             [
+                'WPS Hide Login',
+                'WPS Hide Login is a very light plugin that lets you easily' +
+                ' and safely change the url of the login form page to' +
+                ' anything you want. It doesn\'t literally rename or change' +
+                ' files in core, nor does it add rewrite rules. It simply' +
+                ' intercepts page requests and works on any WordPress' +
+                ' website. The wp-admin directory and wp-login.php page' +
+                ' become inaccessible, so you should bookmark or remember' +
+                ' the url. Deactivating this plugin brings your site back' +
+                ' exactly to the state it was before.',
+                'https://wordpress.org/plugins/wps-hide-login/'
+            ],
+            [
                 'WP Security Scan',
                 'Scans your WordPress installation for security' +
                 ' vulnerabilities.',
